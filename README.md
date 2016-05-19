d5fc235924d6df3404d45de4f0e6d0f24f955fb7
Lee <lee@leehodge.com>
2016-05-19 11:46:03 -0400

    Second Commit

--- README.md
+++
@@ -1,3 +0,0 @@
-"# NCI"
-TEST
-ONE
--- createRespository.bat
+++
@@ -1,6 +0,0 @@
-echo "# NCI" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git remote add origin https://github.com/LeeHodge/NCI.git
-git push -u origin master
